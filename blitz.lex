5b6f1f6e70243e7ab13c08f0edbd07971e213ba9
Safwan Shahab <79778141+SafwanShahab@users.noreply.github.com>
2024-06-08 23:25:46 -0700

    Add files via upload

--- blitz.lex
+++ blitz.lex
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Declare the global variables as extern
+extern int brace_count;
+extern int paren_count;
+
 int line_num = 1;
 int col_num = 1;
 %}
@@ -12,42 +16,41 @@ DIGIT   [0-9]
 LETTER  [a-zA-Z]
 ID      {LETTER}({LETTER}|{DIGIT})*
 NUM     {DIGIT}+
-INVALID_ID {DIGIT}{LETTER}({LETTER}|{DIGIT})*
 
 %%
-"int"                           { printf("KEYWORD_INT\n"); col_num += yyleng; }
-"num"                           { printf("KEYWORD_NUM\n"); col_num += yyleng; }
-"if"                            { printf("KEYWORD_IF\n"); col_num += yyleng; }
-"else"                          { printf("KEYWORD_ELSE\n"); col_num += yyleng; }
-"while"                         { printf("KEYWORD_WHILE\n"); col_num += yyleng; }
-"stop"                          { printf("KEYWORD_STOP\n"); col_num += yyleng; }
-"read"                          { printf("KEYWORD_READ\n"); col_num += yyleng; }
-"write"                         { printf("KEYWORD_WRITE\n"); col_num += yyleng; }
-"+"                             { printf("OP_PLUS\n"); col_num += yyleng; }
-"-"                             { printf("OP_MINUS\n"); col_num += yyleng; }
-"*"                             { printf("OP_MULTIPLY\n"); col_num += yyleng; }
-"/"                             { printf("OP_DIVIDE\n"); col_num += yyleng; }
-":="                            { printf("OP_ASSIGN\n"); col_num += yyleng; }
-"="                             { printf("OP_EQUAL\n"); col_num += yyleng; }
-"=="                            { printf("OP_EQUAL_EQUAL\n"); col_num += yyleng; }
-"!="                            { printf("OP_NOT_EQUAL\n"); col_num += yyleng; }
-"<"                             { printf("OP_LESS_THAN\n"); col_num += yyleng; }
-">"                             { printf("OP_GREATER_THAN\n"); col_num += yyleng; }
-"<="                            { printf("OP_LESS_EQUAL\n"); col_num += yyleng; }
-">="                            { printf("OP_GREATER_EQUAL\n"); col_num += yyleng; }
-";"                             { printf("SEMICOLON\n"); col_num += yyleng; }
-","                             { printf("COMMA\n"); col_num += yyleng; }
-"("                             { printf("LPAREN\n"); col_num += yyleng; }
-")"                             { printf("RPAREN\n"); col_num += yyleng; }
-"{"                             { printf("LBRACE\n"); col_num += yyleng; }
-"}"                             { printf("RBRACE\n"); col_num += yyleng; }
-{ID}                            { printf("IDENTIFIER (%s)\n", yytext); col_num += yyleng; }
-{NUM}                           { printf("NUMBER (%s)\n", yytext); col_num += yyleng; }
-{INVALID_ID}                    { printf("Error at line %d, column %d: identifier \"%s\" must begin with a letter\n", line_num, col_num, yytext); exit(1); }
-"//".*                          { /* Ignore comments */ }
-"\\n"                           { line_num++; col_num = 1; }
+"void"                          { col_num += yyleng; printf("KEYWORD_VOID\n"); }
+"int"                           { col_num += yyleng; printf("KEYWORD_INT\n"); }
+"num"                           { col_num += yyleng; printf("KEYWORD_NUM\n"); }
+"if"                            { col_num += yyleng; printf("KEYWORD_IF\n"); }
+"else"                          { col_num += yyleng; printf("KEYWORD_ELSE\n"); }
+"while"                         { col_num += yyleng; printf("KEYWORD_WHILE\n"); }
+"stop"                          { col_num += yyleng; printf("KEYWORD_STOP\n"); }
+"read"                          { col_num += yyleng; printf("KEYWORD_READ\n"); }
+"write"                         { col_num += yyleng; printf("KEYWORD_WRITE\n"); }
+"+"                             { col_num += yyleng; printf("OP_PLUS\n"); }
+"-"                             { col_num += yyleng; printf("OP_MINUS\n"); }
+"*"                             { col_num += yyleng; printf("OP_MULTIPLY\n"); }
+"/"                             { col_num += yyleng; printf("OP_DIVIDE\n"); }
+":="                            { col_num += yyleng; printf("OP_ASSIGN\n"); }
+"="                             { col_num += yyleng; printf("OP_EQUAL\n"); }
+"=="                            { col_num += yyleng; printf("OP_EQUAL_EQUAL\n"); }
+"!="                            { col_num += yyleng; printf("OP_NOT_EQUAL\n"); }
+"<"                             { col_num += yyleng; printf("OP_LESS_THAN\n"); }
+">"                             { col_num += yyleng; printf("OP_GREATER_THAN\n"); }
+"<="                            { col_num += yyleng; printf("OP_LESS_EQUAL\n"); }
+">="                            { col_num += yyleng; printf("OP_GREATER_EQUAL\n"); }
+";"                             { col_num += yyleng; printf("SEMICOLON\n"); }
+","                             { col_num += yyleng; printf("COMMA\n"); }
+"("                             { col_num += yyleng; printf("LPAREN\n"); paren_count++; }
+")"                             { col_num += yyleng; printf("RPAREN\n"); paren_count--; }
+"{"                             { col_num += yyleng; printf("LBRACE\n"); brace_count++; }
+"}"                             { col_num += yyleng; printf("RBRACE\n"); brace_count--; }
+{ID}                            { col_num += yyleng; printf("IDENTIFIER (%s)\n", yytext); }
+{NUM}                           { col_num += yyleng; printf("NUMBER (%s)\n", yytext); }
+"//".*                          { col_num += yyleng; /* Ignore comments */ }
+"\n"                            { line_num++; col_num = 1; }
 [ \t\r]+                        { col_num += yyleng; /* Ignore whitespace */ }
-.                               { printf("Error at line %d, column %d: unrecognized symbol \"%s\"\n", line_num, col_num, yytext); exit(1); }
+.                               { printf("Error: unrecognized symbol \"%s\" at line %d, column %d\n", yytext, line_num, col_num); col_num += yyleng; exit(1); }
 %%
 int yywrap() {
     return 1;
--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 34
-#define YY_END_OF_BUFFER 35
+#define YY_NUM_RULES 36
+#define YY_END_OF_BUFFER 37
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,15 +360,15 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[66] =
+static const flex_int16_t yy_accept[69] =
     {   0,
-        0,    0,   35,   33,   31,   34,   33,   23,   24,   11,
-        9,   22,   10,   12,   28,   33,   21,   17,   14,   18,
-       27,   33,   27,   27,   27,   27,   27,   27,   25,   26,
-       31,   16,   29,   28,   32,   13,   19,   15,   20,   27,
-       30,   27,    3,   27,   27,   27,   27,   27,   27,   29,
-       27,    1,    2,   27,   27,   27,   27,    4,    7,    6,
-       27,   27,    5,    8,    0
+        0,    0,   37,   35,   34,   33,   35,   24,   25,   12,
+       10,   23,   11,   13,   31,   35,   22,   18,   15,   19,
+       30,   28,   29,   30,   30,   30,   30,   30,   30,   30,
+       26,   27,   34,   17,   32,   31,   14,   20,   16,   21,
+       30,   30,    4,   30,   30,   30,   30,   30,   30,   30,
+       32,   30,    2,    3,   30,   30,   30,   30,   30,    5,
+        8,    7,    1,   30,   30,    6,    9,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -382,11 +382,11 @@ static const YY_CHAR yy_ec[256] =
        16,   17,    1,    1,   18,   18,   18,   18,   18,   18,
        18,   18,   18,   18,   18,   18,   18,   18,   18,   18,
        18,   18,   18,   18,   18,   18,   18,   18,   18,   18,
-        1,   19,    1,    1,    1,    1,   20,   18,   18,   21,
+       19,    1,   20,    1,    1,    1,   21,   18,   18,   22,
 
-       22,   23,   18,   24,   25,   18,   18,   26,   27,   28,
-       29,   30,   18,   31,   32,   33,   34,   18,   35,   18,
-       18,   18,   36,    1,   37,    1,    1,    1,    1,    1,
+       23,   24,   18,   25,   26,   18,   18,   27,   28,   29,
+       30,   31,   18,   32,   33,   34,   35,   36,   37,   18,
+       18,   18,   38,    1,   39,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -403,68 +403,68 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[38] =
+static const YY_CHAR yy_meta[40] =
     {   0,
         1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    1,    3,    1,    3,
+        1,    3,    1,    1,    1,    1,    1,    3,    1,    1,
         3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    1,    1
+        3,    3,    3,    3,    3,    3,    3,    1,    1
     } ;
 
-static const flex_int16_t yy_base[69] =
+static const flex_int16_t yy_base[71] =
     {   0,
-        0,    0,   76,   77,   73,   77,   58,   77,   77,   77,
-       77,   77,   77,   62,   60,   55,   77,   54,   53,   52,
-        0,   39,   40,   15,   31,   42,   30,   15,   77,   77,
-       60,   77,    0,   49,   48,   77,   77,   77,   77,    0,
-       77,   27,    0,   25,   30,   36,   26,   29,   28,    0,
-       30,    0,    0,   30,   20,   22,   12,    0,    0,    0,
-       22,   20,    0,    0,   77,   38,   37,   46
+        0,    0,   78,   79,   75,   79,   60,   79,   79,   79,
+       79,   79,   79,   64,   62,   57,   79,   56,   55,   54,
+        0,   79,   79,   42,   16,   33,   44,   32,   35,   16,
+       79,   79,   62,   79,    0,   51,   79,   79,   79,   79,
+        0,   29,    0,   27,   32,   38,   28,   31,   30,   29,
+        0,   31,    0,    0,   31,   21,   28,   20,   12,    0,
+        0,    0,    0,   21,   20,    0,    0,   79,   39,   48
     } ;
 
-static const flex_int16_t yy_def[69] =
+static const flex_int16_t yy_def[71] =
     {   0,
-       65,    1,   65,   65,   65,   65,   65,   65,   65,   65,
-       65,   65,   65,   65,   66,   65,   65,   65,   65,   65,
-       67,   65,   67,   67,   67,   67,   67,   67,   65,   65,
-       65,   65,   68,   66,   66,   65,   65,   65,   65,   67,
-       65,   67,   67,   67,   67,   67,   67,   67,   67,   68,
-       67,   67,   67,   67,   67,   67,   67,   67,   67,   67,
-       67,   67,   67,   67,    0,   65,   65,   65
+       68,    1,   68,   68,   68,   68,   68,   68,   68,   68,
+       68,   68,   68,   68,   68,   68,   68,   68,   68,   68,
+       69,   68,   68,   69,   69,   69,   69,   69,   69,   69,
+       68,   68,   68,   68,   70,   68,   68,   68,   68,   68,
+       69,   69,   69,   69,   69,   69,   69,   69,   69,   69,
+       70,   69,   69,   69,   69,   69,   69,   69,   69,   69,
+       69,   69,   69,   69,   69,   69,   69,    0,   68,   68
     } ;
 
-static const flex_int16_t yy_nxt[115] =
+static const flex_int16_t yy_nxt[119] =
     {   0,
         4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
-       14,   15,   16,   17,   18,   19,   20,   21,   22,   21,
-       21,   23,   21,   21,   24,   21,   21,   25,   21,   21,
-       26,   27,   21,   21,   28,   29,   30,   43,   48,   40,
-       35,   64,   44,   63,   62,   49,   50,   61,   50,   60,
-       59,   58,   57,   56,   55,   54,   53,   52,   51,   65,
-       34,   31,   47,   46,   45,   42,   41,   39,   38,   37,
-       36,   34,   33,   32,   31,   65,    3,   65,   65,   65,
-       65,   65,   65,   65,   65,   65,   65,   65,   65,   65,
-       65,   65,   65,   65,   65,   65,   65,   65,   65,   65,
+       14,   15,   16,   17,   18,   19,   20,   21,   22,   23,
+       21,   21,   24,   21,   21,   25,   21,   21,   26,   21,
+       21,   27,   28,   21,   21,   29,   30,   31,   32,   43,
+       49,   41,   67,   66,   44,   65,   64,   50,   51,   63,
+       51,   62,   61,   60,   59,   58,   57,   56,   55,   54,
+       53,   52,   36,   33,   48,   47,   46,   45,   42,   40,
+       39,   38,   37,   36,   35,   34,   33,   68,    3,   68,
+       68,   68,   68,   68,   68,   68,   68,   68,   68,   68,
+       68,   68,   68,   68,   68,   68,   68,   68,   68,   68,
 
-       65,   65,   65,   65,   65,   65,   65,   65,   65,   65,
-       65,   65,   65,   65
+       68,   68,   68,   68,   68,   68,   68,   68,   68,   68,
+       68,   68,   68,   68,   68,   68,   68,   68
     } ;
 
-static const flex_int16_t yy_chk[115] =
+static const flex_int16_t yy_chk[119] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,   24,   28,   67,
-       66,   62,   24,   61,   57,   28,   68,   56,   68,   55,
-       54,   51,   49,   48,   47,   46,   45,   44,   42,   35,
-       34,   31,   27,   26,   25,   23,   22,   20,   19,   18,
-       16,   15,   14,    7,    5,    3,   65,   65,   65,   65,
-       65,   65,   65,   65,   65,   65,   65,   65,   65,   65,
-       65,   65,   65,   65,   65,   65,   65,   65,   65,   65,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,   25,
+       30,   69,   65,   64,   25,   59,   58,   30,   70,   57,
+       70,   56,   55,   52,   50,   49,   48,   47,   46,   45,
+       44,   42,   36,   33,   29,   28,   27,   26,   24,   20,
+       19,   18,   16,   15,   14,    7,    5,    3,   68,   68,
+       68,   68,   68,   68,   68,   68,   68,   68,   68,   68,
+       68,   68,   68,   68,   68,   68,   68,   68,   68,   68,
 
-       65,   65,   65,   65,   65,   65,   65,   65,   65,   65,
-       65,   65,   65,   65
+       68,   68,   68,   68,   68,   68,   68,   68,   68,   68,
+       68,   68,   68,   68,   68,   68,   68,   68
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -481,15 +481,17 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "blitz.l"
-#line 2 "blitz.l"
-#include "blitz.tab.h"
+#line 1 "blitz.lex"
+#line 2 "blitz.lex"
 #include <stdio.h>
+#include <stdlib.h>
 
 int line_num = 1;
 int col_num = 1;
-#line 491 "lex.yy.c"
-#line 492 "lex.yy.c"
+
+void yyerror(const char* s);
+#line 493 "lex.yy.c"
+#line 494 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -706,10 +708,9 @@ YY_DECL
 		}
 
 	{
-#line 14 "blitz.l"
+#line 16 "blitz.lex"
 
-
-#line 712 "lex.yy.c"
+#line 713 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -736,13 +737,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 66 )
+				if ( yy_current_state >= 69 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 77 );
+		while ( yy_base[yy_current_state] != 79 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -768,181 +769,186 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 16 "blitz.l"
-{ col_num += yyleng; printf("Token: int\n"); return KEYWORD_INT; }
+#line 17 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_VOID\n"); }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 17 "blitz.l"
-{ col_num += yyleng; printf("Token: num\n"); return KEYWORD_NUM; }
+#line 18 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_INT\n"); }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 18 "blitz.l"
-{ col_num += yyleng; printf("Token: if\n"); return KEYWORD_IF; }
+#line 19 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_NUM\n"); }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 19 "blitz.l"
-{ col_num += yyleng; printf("Token: else\n"); return KEYWORD_ELSE; }
+#line 20 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_IF\n"); }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 20 "blitz.l"
-{ col_num += yyleng; printf("Token: while\n"); return KEYWORD_WHILE; }
+#line 21 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_ELSE\n"); }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 21 "blitz.l"
-{ col_num += yyleng; printf("Token: stop\n"); return KEYWORD_STOP; }
+#line 22 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_WHILE\n"); }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 22 "blitz.l"
-{ col_num += yyleng; printf("Token: read\n"); return KEYWORD_READ; }
+#line 23 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_STOP\n"); }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 23 "blitz.l"
-{ col_num += yyleng; printf("Token: write\n"); return KEYWORD_WRITE; }
+#line 24 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_READ\n"); }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 24 "blitz.l"
-{ col_num += yyleng; printf("Token: +\n"); return OP_PLUS; }
+#line 25 "blitz.lex"
+{ col_num += yyleng; printf("KEYWORD_WRITE\n"); }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 25 "blitz.l"
-{ col_num += yyleng; printf("Token: -\n"); return OP_MINUS; }
+#line 26 "blitz.lex"
+{ col_num += yyleng; printf("OP_PLUS\n"); }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 26 "blitz.l"
-{ col_num += yyleng; printf("Token: *\n"); return OP_MULTIPLY; }
+#line 27 "blitz.lex"
+{ col_num += yyleng; printf("OP_MINUS\n"); }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 27 "blitz.l"
-{ col_num += yyleng; printf("Token: /\n"); return OP_DIVIDE; }
+#line 28 "blitz.lex"
+{ col_num += yyleng; printf("OP_MULTIPLY\n"); }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 28 "blitz.l"
-{ col_num += yyleng; printf("Token: :=\n"); return OP_ASSIGN; }
+#line 29 "blitz.lex"
+{ col_num += yyleng; printf("OP_DIVIDE\n"); }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 29 "blitz.l"
-{ col_num += yyleng; printf("Token: =\n"); return OP_EQUAL; }
+#line 30 "blitz.lex"
+{ col_num += yyleng; printf("OP_ASSIGN\n"); }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 30 "blitz.l"
-{ col_num += yyleng; printf("Token: ==\n"); return OP_EQUAL_EQUAL; }
+#line 31 "blitz.lex"
+{ col_num += yyleng; printf("OP_EQUAL\n"); }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 31 "blitz.l"
-{ col_num += yyleng; printf("Token: !=\n"); return OP_NOT_EQUAL; }
+#line 32 "blitz.lex"
+{ col_num += yyleng; printf("OP_EQUAL_EQUAL\n"); }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 32 "blitz.l"
-{ col_num += yyleng; printf("Token: <\n"); return OP_LESS_THAN; }
+#line 33 "blitz.lex"
+{ col_num += yyleng; printf("OP_NOT_EQUAL\n"); }
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 33 "blitz.l"
-{ col_num += yyleng; printf("Token: >\n"); return OP_GREATER_THAN; }
+#line 34 "blitz.lex"
+{ col_num += yyleng; printf("OP_LESS_THAN\n"); }
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 34 "blitz.l"
-{ col_num += yyleng; printf("Token: <=\n"); return OP_LESS_EQUAL; }
+#line 35 "blitz.lex"
+{ col_num += yyleng; printf("OP_GREATER_THAN\n"); }
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 35 "blitz.l"
-{ col_num += yyleng; printf("Token: >=\n"); return OP_GREATER_EQUAL; }
+#line 36 "blitz.lex"
+{ col_num += yyleng; printf("OP_LESS_EQUAL\n"); }
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 36 "blitz.l"
-{ col_num += yyleng; printf("Token: ;\n"); return SEMICOLON; }
+#line 37 "blitz.lex"
+{ col_num += yyleng; printf("OP_GREATER_EQUAL\n"); }
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 37 "blitz.l"
-{ col_num += yyleng; printf("Token: ,\n"); return COMMA; }
+#line 38 "blitz.lex"
+{ col_num += yyleng; printf("SEMICOLON\n"); }
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 38 "blitz.l"
-{ col_num += yyleng; printf("Token: (\n"); return LPAREN; }
+#line 39 "blitz.lex"
+{ col_num += yyleng; printf("COMMA\n"); }
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 39 "blitz.l"
-{ col_num += yyleng; printf("Token: )\n"); return RPAREN; }
+#line 40 "blitz.lex"
+{ col_num += yyleng; printf("LPAREN\n"); }
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 40 "blitz.l"
-{ col_num += yyleng; printf("Token: {\n"); return LBRACE; }
+#line 41 "blitz.lex"
+{ col_num += yyleng; printf("RPAREN\n"); }
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 41 "blitz.l"
-{ col_num += yyleng; printf("Token: }\n"); return RBRACE; }
+#line 42 "blitz.lex"
+{ col_num += yyleng; printf("LBRACE\n"); }
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 42 "blitz.l"
-{ col_num += yyleng; printf("Token: identifier (%s)\n", yytext); yylval.str = strdup(yytext); return IDENTIFIER; }
+#line 43 "blitz.lex"
+{ col_num += yyleng; printf("RBRACE\n"); }
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 43 "blitz.l"
-{ col_num += yyleng; printf("Token: number (%s)\n", yytext); yylval.num = atoi(yytext); return NUMBER; }
+#line 44 "blitz.lex"
+{ col_num += yyleng; printf("LBRACKET\n"); }
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 44 "blitz.l"
-{ /* Ignore comments */ }
+#line 45 "blitz.lex"
+{ col_num += yyleng; printf("RBRACKET\n"); }
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 45 "blitz.l"
-{ line_num++; col_num = 1; }
+#line 46 "blitz.lex"
+{ col_num += yyleng; printf("IDENTIFIER (%s)\n", yytext); }
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 46 "blitz.l"
-{ col_num += yyleng; /* Ignore whitespace */ }
+#line 47 "blitz.lex"
+{ col_num += yyleng; printf("NUMBER (%s)\n", yytext); }
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 47 "blitz.l"
-{ 
-                                  fprintf(stderr, "Error at line %d, column %d: identifier \"%s\" must begin with a letter\n", line_num, col_num, yytext); 
-                                  exit(1); 
-                                }
+#line 48 "blitz.lex"
+{ col_num += yyleng; /* Ignore comments */ }
 	YY_BREAK
 case 33:
+/* rule 33 can match eol */
 YY_RULE_SETUP
-#line 51 "blitz.l"
-{ 
-                                  fprintf(stderr, "Error at line %d, column %d: unrecognized symbol \"%s\"\n", line_num, col_num, yytext); 
-                                  exit(1); 
-                                }
+#line 49 "blitz.lex"
+{ line_num++; col_num = 1; }
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 55 "blitz.l"
+#line 50 "blitz.lex"
+{ col_num += yyleng; /* Ignore whitespace */ }
+	YY_BREAK
+case 35:
+YY_RULE_SETUP
+#line 51 "blitz.lex"
+{ printf("Error at line %d, column %d: unrecognized symbol \"%s\"\n", line_num, col_num, yytext); col_num += yyleng; exit(1); }
+	YY_BREAK
+case 36:
+YY_RULE_SETUP
+#line 52 "blitz.lex"
 ECHO;
 	YY_BREAK
-#line 945 "lex.yy.c"
+#line 951 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1239,7 +1245,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 66 )
+			if ( yy_current_state >= 69 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1267,11 +1273,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 66 )
+		if ( yy_current_state >= 69 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 65);
+	yy_is_jam = (yy_current_state == 68);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1947,10 +1953,22 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 55 "blitz.l"
+#line 52 "blitz.lex"
 
 
-int yywrap() {
-    return 1;
+void yyerror(const char* s) {
+    fprintf(stderr, "Error at line %d, column %d: %s\n", line_num, col_num, s);
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    ++argv, --argc;  /* skip over program name */
+    if (argc > 0)
+        yyin = fopen(argv[0], "r");
+    else
+        yyin = stdin;
+
+    yylex();
+    return 0;
 }
 
